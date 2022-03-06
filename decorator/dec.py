06a1eb85dcde1d330bf33ef7ad0926c1a5fc136d
a-povazhnyi <a.povazhnyi@wis.software>
2022-03-06 20:07:38 +0300

    fix: add logging STDOUT instead of print

--- decorator/dec.py
+++ decorator/dec.py
@@ -1,17 +1,25 @@
 from datetime import datetime
+import logging
+
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+console = logging.StreamHandler()
+log.addHandler(console)
 
 
 def timeit(func):
     def wrapper(*args, **kwargs):
         start_time = datetime.utcnow()
-        print(f'Start Time: {start_time.strftime("%H:%M:%S.%f")}')
+        log.info(f'Start Time: {start_time.strftime("%H:%M:%S.%f")}')
 
         f = func(*args, **kwargs)
 
         end_time = datetime.utcnow()
-        print(f'End Time: {end_time.strftime("%H:%M:%S.%f")}')
+        log.info(f'Start Time: {end_time.strftime("%H:%M:%S.%f")}')
 
         time = end_time - start_time
-        print(f'|{func.__name__}| Time Duration: {time}')
+        log.info(f'|{func.__name__}| Time Duration: {time}')
         return f
     return wrapper
