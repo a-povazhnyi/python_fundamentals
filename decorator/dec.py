287ab3a6e89343123cd81212b554d4ca594c454d
a-povazhnyi <a.povazhnyi@wis.software>
2022-03-06 19:13:51 +0300

    fix: rename decorator and replace datetime.now()

--- decorator/dec.py
+++ decorator/dec.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 
 
-def timeit2(func):
+def timeit(func):
     def wrapper(*args, **kwargs):
-        start_time = datetime.now()
+        start_time = datetime.utcnow()
         print(f'Start Time: {start_time.strftime("%H:%M:%S.%f")}')
 
         f = func(*args, **kwargs)
 
-        end_time = datetime.now()
+        end_time = datetime.utcnow()
         print(f'End Time: {end_time.strftime("%H:%M:%S.%f")}')
 
         time = end_time - start_time
