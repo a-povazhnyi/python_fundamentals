b9f5ec4002a852e41cbd50de2d852e8dfe6c3cbf
a-povazhnyi <a.povazhnyi@wis.software>
2022-03-06 20:20:53 +0300

    raise of error to the top; add comment

--- string_to_int/str_to_int.py
+++ string_to_int/str_to_int.py
@@ -1,7 +1,11 @@
 def str_to_int(line):
-    if isinstance(line, str) and line.isnumeric():
+    if not isinstance(line, str) and not line.isnumeric():
+        raise TypeError('Argument type should be a numeric string.')
+    else:
         result = 0
         for digit in line:
+            # ord() returns an integer representing the Unicode
+            # code point of the character
+            # 48 is a number where digits start
             result = result * 10 + (ord(digit) - 48)
         return result
-    raise TypeError('Argument type should be a numeric string.')
