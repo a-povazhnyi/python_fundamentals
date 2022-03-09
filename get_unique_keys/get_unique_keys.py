6b15638bc11ef9ecefddb1af95a7e0d1e037abc9
a-povazhnyi <a.povazhnyi@wis.software>
2022-03-09 12:40:59 +0300

    fix: add optimization

--- get_unique_keys/get_unique_keys.py
+++ get_unique_keys/get_unique_keys.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 # 1
 def get_unique_keys(structure):
     used = []
@@ -9,73 +12,60 @@ def get_unique_keys(structure):
 
 # 2
 def get_unique_keys_by_pop(structure):
-    used = {}
+    used = Counter()
 
     for dish in structure['Cook Book']:
         dish_values = list(*dish.values())
 
         for value in dish_values:
-            if value not in used.keys():
-                used[value] = 1
-            else:
-                used[value] += 1
+            used[value] += 1
 
-    sorted_used = sorted(used, key=used.get, reverse=True)
-    return sorted_used
+    return list(used.keys())
 
 
 # 3
 class UniqueKeys:
     def __init__(self, structure, by_pop=False):
-        self._structure = structure
+        self._structure = structure['Cook Book']
         self._by_pop = by_pop
-        self._current_index = -1
+        self._current_index = 0
+        self._used = self.make_unique_keys(self._structure)
 
-        if not self._by_pop:
-            self._used = self.make_unique_keys()
-            self._limit = len(self._used) - 1
-        else:
-            self._sorted_used = self.make_unique_keys_by_pop()
-            self._limit = len(self._sorted_used) - 1
+        if self._by_pop:
+            self._used = self.make_unique_keys_by_pop(self._structure)
+
+        self._limit = len(self._used) - 1
 
     def __iter__(self):
         return self
 
     def __next__(self):
+        if self._current_index > self._limit:
+            raise StopIteration
+
+        result = self._used[self._current_index]
         self._current_index += 1
+        return result
 
-        if not self._by_pop:
-            if self._current_index > self._limit:
-                raise StopIteration
-            else:
-                return self._used[self._current_index]
-        else:
-            if self._current_index > self._limit:
-                raise StopIteration
-            else:
-                return self._sorted_used[self._current_index]
-
-    def make_unique_keys(self):
+    @staticmethod
+    def make_unique_keys(structure):
         used = []
 
-        for dish in self._structure['Cook Book']:
+        for dish in structure:
             used.extend(*dish.values())
         return list(set(used))
 
-    def make_unique_keys_by_pop(self):
-        used = {}
+    @staticmethod
+    def make_unique_keys_by_pop(structure):
+        used = Counter()
 
-        for dish in self._structure['Cook Book']:
+        for dish in structure:
             dish_values = list(*dish.values())
 
             for value in dish_values:
-                if value not in used.keys():
-                    used[value] = 1
-                else:
-                    used[value] += 1
+                used[value] += 1
 
-        sorted_used = sorted(used, key=used.get, reverse=True)
-        return sorted_used
+        return list(used.keys())
 
 
 cook_book = {
