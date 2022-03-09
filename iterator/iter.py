f6a82a786b0632faa72be0b67db52aff44d499a3
a-povazhnyi <a.povazhnyi@wis.software>
2022-03-09 12:02:04 +0300

    fix: add result variable; change _current_index

--- iterator/iter.py
+++ iterator/iter.py
@@ -2,15 +2,15 @@ class Iterator:
     def __init__(self, sample: str):
         self._sample = sample
         self._limit = len(sample) - 1
-        self._current_index = -1
+        self._current_index = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        self._current_index += 1
-
         if self._current_index > self._limit:
             raise StopIteration
-        else:
-            return self._sample[self._current_index]
+
+        result = self._sample[self._current_index]
+        self._current_index += 1
+        return result
